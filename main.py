5087d8f8bdc4b1f472f3b567acc7f499607ab5e7
SoldierDown <dyhard0520@gmail.com>
2021-09-07 21:55:42 -0400

    finish 1st version of na, la, ga, union and harbor

--- main.py
+++ main.py
@@ -28,28 +28,67 @@ class AutoRun(object):
     def check_exists(self, img_name="", base_path="./img"):
         return False
 
+    def find_and_click(self, img_path='', name='', offset=[0, 0], ind=1, n_clicks=1, pause=SHORT_PAUSE):
+        conf = MAX_CONF
+        pos = pag.locateOnScreen(img_path, confidence=conf)
+        while pos is None and conf > MIN_CONF:
+            conf += DCONF
+            pos = pag.locateOnScreen(img_path, confidence=conf)
+        if pos is not None:
+            pag.moveTo(pos.left + offset[0], pos.top + offset[1])
+            pag.PAUSE=pause
+            for iter in range(n_clicks):
+                pag.click()
+            pag.PAUSE=SHORT_PAUSE
+            return True, pos.left, pos.top
+        else:
+            output=''
+            for iter in range(ind):
+                output +='\t'
+            output+=name
+            output+='未找到'
+            print(output)
+            return False, 0, 0
+    
+    def drag_find_and_click(self, fp=[0, 0], dragto=[0, 0], img_path='', name='', offset=[0, 0],ind=1, n_clicks=1, n_drags=1):
+        if img_path == '':
+            for iter in range(n_drags):
+                pag.moveTo(fp[0], fp[1])
+                pag.drag(dragto[0], dragto[1])
+                return True, 0, 0
+        pag.moveTo(fp[0], fp[1])
+        conf = MAX_CONF
+        pos = pag.locateOnScreen(img_path, confidence=conf)
+        while pos is None and conf > MIN_CONF:
+            pag.drag(dragto[0], dragto[1])
+            conf += DCONF
+            pos = pag.locateOnScreen(img_path, confidence=conf)
+            pag.moveTo(fp[0], fp[1])
+        if pos is not None:
+            pag.moveTo(pos.left + offset[0], pos.top + offset[1])
+            for iter in range(n_clicks):
+                pag.click()
+            return True, pos.left, pos.top
+        else:
+            output=''
+            for iter in range(ind):
+                output +='\t'
+            output+=name
+            output+='未找到'
+            print(output)
+            return False, 0, 0
+
     def run(self):
         ''''''
         self.normal_activity()
         self.time_limited_activity()
+        self.game_assistant()
+        self.harbor()
         self.union()
     
     def back_to_home(self):
-        ''''''
-        conf_bth = MAX_CONF
-        pos_bth = pag.locateOnScreen('./img/bth.png', confidence=conf_bth)
-        while pos_bth is None and conf_bth > MIN_CONF:
-            conf_bth += DCONF
-            pos_bth = pag.locateOnScreen('./img/bth.png', confidence=conf_bth)
-        if pos_bth is not None:
-            pag.moveTo(pos_bth.left, pos_bth.top)
-            cnt = 0
-            while cnt < N_CLICKS:
-                pag.click()
-                cnt += 1
-        else:
-            print('主页未找到')
-            exit()
+        ''' 回到主页 '''
+        self.find_and_click(img_path='./img/bth.png', name='主页', n_clicks=N_CLICKS)
 
     def normal_activity(self):
         ''' 日常任务 '''
@@ -59,225 +98,208 @@ class AutoRun(object):
         self.get_vip_gift()
         self.get_daily_gift()
         print('日常任务完成')
-
     def daily_checkin(self):
         ''' 每日签到 '''
         self.back_to_home()
         finished = False
         print('    每日签到开始')
-        conf_na = MAX_CONF
-        pos_na = pag.locateOnScreen('./img/na.png', confidence=conf_na)
-        while pos_na is None and conf_na > MIN_CONF:
-            conf_na += DCONF
-            pos_na = pag.locateOnScreen('./img/na.png', confidence=conf_na)
-        if pos_na is not None:
-            pag.moveTo(pos_na.left, pos_na.top)
-            pag.click()
-
-            conf_dci = MAX_CONF
-            pos_dci = pag.locateOnScreen('./img/na_dci.png', confidence=conf_dci)
-            while pos_dci is None and conf_dci > MIN_CONF:
-                conf_dci += DCONF
-                pos_dci = pag.locateOnScreen('./img/na_dci.png', confidence=conf_dci)
-            if pos_dci is not None:
-                pag.moveTo(pos_dci.left, pos_dci.top)
-                pag.click()
-
-                conf_aci = MAX_CONF
-                pos_aci = pag.locateOnScreen('./img/na_dci_aci.png', confidence=conf_aci)
-                while pos_aci is None and conf_aci > MIN_CONF:
-                    conf_aci += DCONF
-                    pos_aci = pag.locateOnScreen('./img/na_dci_aci.png', confidence=conf_aci)
-                if pos_aci is not None:
-                    pag.moveTo(pos_aci.left, pos_aci.top)
-                    pag.click()
-
-                    conf_lq = MAX_CONF
-                    pos_lq = pag.locateOnScreen('./img/na_dci_aci_lq.png', confidence=conf_lq)
-                    while pos_lq is None and conf_lq > MIN_CONF:
-                        conf_lq += DCONF
-                        pos_lq = pag.locateOnScreen('./img/na_dci_aci_lq.png', confidence=conf_lq)
-                    if pos_lq is not None:
-                        pag.moveTo(pos_lq.left, pos_lq.top)
-                        pag.click()
-                        finished = True
-                    else:
-                        print('    每日签到领取未找到')
-                else:
-                    print('    每日签到格未找到')
-            else:
-                print('    每日签到未找到')
-        else:
-            print('    日常活动未找到')
+        self.find_and_click(img_path='./img/na.png', name='日常活动')
+        self.find_and_click(img_path='./img/na_dci.png', name='每日签到')
+        self.find_and_click(img_path='./img/na_dci_aci.png', name='每日签到格')
+        finished, _, _=self.find_and_click(img_path='./img/na_dci_aci_lq.png.bak', name='每日签到领取')
         if finished:
             print('    每日签到完成')
-
     def buy_bali(self):
         ''' 购买贝里 '''
         self.back_to_home()
         finished = False
         print('    购买贝里开始')
-        conf_na = MAX_CONF
-        pos_na = pag.locateOnScreen('./img/na.png', confidence=conf_na)
-        while pos_na is None and conf_na > MIN_CONF:
-            conf_na += DCONF
-            pos_na = pag.locateOnScreen('./img/na.png', confidence=conf_na)
-        if pos_na is not None:
-            pag.moveTo(pos_na.left, pos_na.top)
-            pag.click()
-
-            # fixed point for dragging
-            conf_fp = MAX_CONF
-            pos_fp = pag.locateOnScreen('./img/na_fp', confidence=conf_fp)
-            while pos_fp is None and conf_fp > MIN_CONF:
-                conf_fp += DCONF
-                pos_fp = pag.locateOnScreen('./img/na_fp', confidence=conf_fp)
-            if pos_fp is not None:
-                pag.moveTo(pos_fp.left + 2 * DPM, pos_fp.top)
-                pos_bb = None
-                conf_bb = MAX_CONF
-                pos_bb = pag.locateOnScreen('./img/na_bb', confidence=conf_bb)
-                while pos_bb is None and conf_bb > MIN_CONF:
-                    pag.drag(-DPM, 0)
-                    conf_bb += DCONF
-                    pos_bb = pag.locateOnScreen('./img/na_bb', confidence=conf_bb)
-                    pag.moveTo(pos_fp.left + 2 * DPM, pos_fp.top)
-                if pos_bb is not None:
-                    pag.moveTo(pos_bb.left, pos_bb.top)
-                    pag.click()
-
-                    conf_bo = MAX_CONF
-                    pos_bo = pag.locateOnScreen('./img/na_bb_bo.png', confidence=conf_bo)
-                    while pos_bo is None and conf_bo > MIN_CONF:
-                        conf_bo += DCONF
-                        pos_bo = pag.locateOnScreen('./img/na_bb_bo.png', confidence=conf_bo)
-                    if pos_bo is not None:
-                        pag.moveTo(pos_bo.left, pos_bo.top)
-                        pag.click()
-                        finished = True
-                    else:
-                        print('    购买贝里一次未找到')
-                else:
-                    print('    购买贝里未找到')
-            else:
-                print('    固定点未找到')
-                exit()
-        else:
-            print('    日常活动未找到')
+        self.find_and_click(img_path='./img/na.png', name='日常活动')
+        _, fpx, fpy = self.find_and_click(img_path='./img/na_fp.png', name='固定点', n_clicks=0)
+        self.drag_find_and_click(fp=[fpx + 2 * DPM, fpy], dragto=[-DPM, 0], img_path='./img/na_bb.png', name='购买贝里')
+        finished, _, _ = self.find_and_click(img_path='./img/na_bb_bo.png', name='购买贝里一次')
         if finished:
             print('    购买贝里完成')
-        
     def get_vip_gift(self):
         ''' VIP礼物 '''
         self.back_to_home()
         finished = False
         print('    VIP礼物开始')
-        conf_na = MAX_CONF
-        pos_na = pag.locateOnScreen('./img/na.png', confidence=conf_na)
-        while pos_na is None and conf_na > MIN_CONF:
-            conf_na += DCONF
-            pos_na = pag.locateOnScreen('./img/na.png', confidence=conf_na)
-        if pos_na is not None:
-            pag.moveTo(pos_na.left, pos_na.top)
-            pag.click()
-
-            # fixed point for dragging
-            conf_fp = MAX_CONF
-            pos_fp = pag.locateOnScreen('./img/na_fp', confidence=conf_fp)
-            while pos_fp is None and conf_fp > MIN_CONF:
-                conf_fp += DCONF
-                pos_fp = pag.locateOnScreen('./img/na_fp', confidence=conf_fp)
-            if pos_fp is not None:
-                pag.moveTo(pos_fp.left + 2 * DPM, pos_fp.top)
-                pos_bb = None
-                conf_bb = MAX_CONF
-                pos_bb = pag.locateOnScreen('./img/na_bb', confidence=conf_bb)
-                while pos_bb is None and conf_bb > MIN_CONF:
-                    pag.drag(-DPM, 0)
-                    conf_bb += DCONF
-                    pos_bb = pag.locateOnScreen('./img/na_bb', confidence=conf_bb)
-                    pag.moveTo(pos_fp.left + 2 * DPM, pos_fp.top)
-                if pos_bb is not None:
-                    pag.moveTo(pos_bb.left, pos_bb.top)
-                    pag.click()
-
-                    conf_bo = MAX_CONF
-                    pos_bo = pag.locateOnScreen('./img/na_bb_bo.png', confidence=conf_bo)
-                    while pos_bo is None and conf_bo > MIN_CONF:
-                        conf_bo += DCONF
-                        pos_bo = pag.locateOnScreen('./img/na_bb_bo.png', confidence=conf_bo)
-                    if pos_bo is not None:
-                        pag.moveTo(pos_bo.left, pos_bo.top)
-                        pag.click()
-                    else:
-                        print('    购买贝里一次未找到')
-                else:
-                    print('    购买贝里未找到')
-            else:
-                print('    固定点未找到')
-                exit()
-        else:
-            print('    日常活动未找到')
+        self.find_and_click(img_path='./img/na.png', name='日常活动')
+        _, fpx, fpy = self.find_and_click(img_path='./img/na_fp.png', name='固定点', n_clicks=0)
+        self.drag_find_and_click(fp=[fpx + 2 * DPM, fpy], dragto=[-DPM, 0], img_path='./img/na_vipg.png', name='VIP礼包')
+        self.find_and_click(img_path='./img/na_vipg_mrg.png', name='VIP每日礼包')
+        finished, _, _ = self.find_and_click(img_path='./img/na_vipg_mrg_lq.png', name='VIP每日礼包领取')
         if finished:
-            print('    购买贝里完成')
-        pass
-
+            print('    VIP每日礼包完成')
     def get_daily_gift(self):
         ''' 日常礼包 '''
-        pass
+        self.back_to_home()
+        finished = False
+        print('    日常礼包开始')
+        self.find_and_click(img_path='./img/na.png', name='日常活动')
+        _, fpx, fpy = self.find_and_click(img_path='./img/na_fp.png', name='固定点', n_clicks=0)
+        self.drag_find_and_click(fp=[fpx + 2 * DPM, fpy], dragto=[-DPM, 0], n_drags=8)
+        self.find_and_click(img_path='./img/na_rcg.png', name='日常礼包')
+        
+        self.find_and_click(img_path='./img/na_rcg_mrg.png', name='日常礼包每日礼包', ind=2)
+        finished, _, _ = self.find_and_click(img_path='./img/na_rcg_mrg_mf.png', name='日常礼包每日礼包领取', ind=2)
+
+        self.find_and_click(img_path='./img/na_rcg_mzg.png', name='日常礼包每周礼包', ind=2)
+        finished, _, _ = self.find_and_click(img_path='./img/na_rcg_mzg_mf.png', name='日常礼包每周礼包领取', ind=2)
+
+        self.find_and_click(img_path='./img/na_rcg_myg.png', name='日常礼包每月礼包', ind=2)
+        finished, _, _ = self.find_and_click(img_path='./img/na_rcg_myg_mf.png', name='日常礼包每月礼包领取', ind=2)
+
+        if finished:
+            print('    日常礼包完成')
+
+
 
     def time_limited_activity(self):
         ''' 限时活动 '''
+        print('限时活动开始')
         self.consecutive_logins()
         self.sales_items()
         self.dollar_shop()
+        print('限时活动完成')
     def consecutive_logins(self):
-        ''' 累积登录 '''
-        pass
-    def sales_items(self):
-        ''' 道具折扣 '''
-        pass
+        ''' 累计登录 '''
+        self.back_to_home()
+        finished = False
+        print('    累计登录开始')
+        self.find_and_click(img_path='./img/la.png', name='限时活动')
+        self.find_and_click(img_path='./img/la_lj.png', name='累计登录')
+        finished, _, _ = self.find_and_click(img_path='./img/la_lj_lq.png', name='累计登录领取', n_clicks=5)
+        if finished:
+            print('    累计登录完成')
     def dollar_shop(self):
         ''' 福利商店 '''
-        pass
-        
+        self.back_to_home()
+        finished = False
+        print('    福利商店开始')
+        self.find_and_click(img_path='./img/la.png', name='限时活动')
+        self.find_and_click(img_path='./img/la_fl.png', name='福利商店')
+        finished, _, _ = self.find_and_click(img_path='./img/la_fl_tl.png', name='福利商店购买体力', offset=[6*DPM, 0.5*DPM], n_clicks=5)
+        if finished:
+            print('    福利商店完成')
+    def sales_items(self):
+        ''' 道具折扣 '''
+        self.back_to_home()
+        finished = False
+        print('    道具折扣开始')
+        self.find_and_click(img_path='./img/la.png', name='限时活动')
+        self.find_and_click(img_path='./img/la_dj.png', name='道具折扣')
+        finished, _, _ = self.find_and_click(img_path='./img/la_dj_tl.png', name='道具折扣购买体力', offset=[6*DPM, 0.5*DPM], n_clicks=5)
+        if finished:
+            print('    道具折扣完成')
+
+
+
+    def game_assistant(self):
+        ''' 游戏助手 '''
+        self.back_to_home()
+        finished = False
+        print('游戏助手开始')
+        self.find_and_click(img_path='./img/ga.png', name='游戏助手')
+        self.find_and_click(img_path='./img/ga_da.png', name='全部执行', pause=LONG_PAUSE)
+        self.find_and_click(img_path='./img/ga_da_back.png', name='游戏执行完成')
+        self.find_and_click(img_path='./img/ga_da_back_back.png', name='退出游戏助手')
+        print('游戏助手完成')
+
+    
+
     def union(self):
         ''' 工会活动 '''
         print('工会活动开始')
         self.union_construction()
         self.pirate_wanted()
+        print('工会活动完成')
+
     def union_construction(self):
         ''' 工会建设 '''
         self.back_to_home()
+        finished = False
         print('    工会建设开始')
-        conf_gh = MAX_CONF
-        pos_gh = pag.locateOnScreen('./img/gh.png', confidence=conf_gh)
-        while pos_gh is None:
-            conf_gh += DCONF
-            pos_gh = pag.locateOnScreen('./img/gh.png', confidence=conf_gh)
-        if pos_gh is not None:
-            pag.moveTo(pos_gh.left, pos_gh.top)
-            # pag.PAUSE = LONG_PAUSE
-            pag.click()
-            pag.PAUSE = SHORT_PAUSE
-            
-            conf_ptjs = MAX_CONF
-            pos_ptjs = pag.locateOnScreen('./img/gh_ptjs.png', confidence=conf_ptjs)
-            while pos_ptjs is None:
-                conf_ptjs += DCONF
-                pos_ptjs = pag.locateOnScreen('./img/gh_ptjs.png', confidence=conf_ptjs)
-            if pos_ptjs is not None:
-                pag.moveTo(pos_ptjs.left, pos_ptjs.top)
-                pag.click()
-            else:
-                print('        海盗悬赏未找到')
-            print('    工会建设完成')
-        else:
-            print('    工会建设未找到')
-        
+        self.find_and_click(img_path='./img/gh.png', name='工会', pause=LONG_PAUSE)
+        self.find_and_click(img_path='./img/gh_ghdt.png', name='工会大厅')
+        self.find_and_click(img_path='./img/gh_ghdt_ptjs.png', name='普通建设', offset=[DPM/3, DPM*3.3])
+        self.find_and_click(img_path='./img/gh_ghdt_fh.png', name='退出工会大厅')
+        finished, _, _ = self.find_and_click(img_path='./img/gh_fh.png', name='退出工会')
+        if finished:
+            print('    道具折扣完成')
     def pirate_wanted(self):
         ''' 海盗悬赏 '''
-        pass
+        self.back_to_home()
+        finished = False
+        print('    海盗悬赏开始')
+        self.find_and_click(img_path='./img/gh.png', name='工会', pause=LONG_PAUSE)
+        self.find_and_click(img_path='./img/gh_hdxs.png', name='海盗悬赏')
+        _, tzx, tzy = self.find_and_click(img_path='./img/gh_hdxs_tz.png', name='海盗悬赏挑战')
+        pag.moveTo(tzx, tzy+2*DPM)
+        pag.click()
+        self.find_and_click(img_path='./img/gh_ghdt_fh.png', name='退出海盗悬赏')
+        finished, _, _ = self.find_and_click(img_path='./img/gh_fh.png', name='退出工会')
+        if finished:
+            print('    海盗悬赏完成')
+
+
+
+    def harbor(self):
+        ''' 港口 '''
+        print('港口开始')
+        self.harbor_reward()
+        self.harbor_shop()
+        print('港口完成')
+    def harbor_reward(self):
+        ''' 港口领奖 '''
+        self.back_to_home()
+        finished = False
+        print('    港口领奖开始')
+        self.find_and_click(img_path='./img/gk.png', name='港口')
+        self.find_and_click(img_path='./img/gk_lj.png', name='港口领奖')
+        finished, _, _ = self.find_and_click(img_path='./img/gk_lj_qd.png', name='港口领奖确定')
+        self.find_and_click(img_path='./img/gk_fh.png', name='退出港口')
+        if finished:
+            print('    港口领奖完成')
+    def harbor_shop(self):
+        ''' 港口商店 '''
+        self.back_to_home()
+        finished = False
+        print('    港口商店开始')
+        self.find_and_click(img_path='./img/gk.png', name='港口')
+        self.find_and_click(img_path='./img/gk_sd.png', name='港口商店')
+        _, fpx, fpy = self.find_and_click(img_path='./img/gk_sd_fp.png', name='固定点', n_clicks=0)
+        finished, _, _ = self.drag_find_and_click(fp=[fpx, fpy+2*DPM], dragto=[0, -2*DPM], offset=[6*DPM, 0.5*DPM], img_path='./img/gk_sd_r.png', name='红色饰品精华')
+        if finished:
+            self.find_and_click(img_path='./img/gk_sd_pt.png', name='+10')
+            self.find_and_click(img_path='./img/gk_sd_r_qd.png', name='确定购买红色饰品精华')
+            self.find_and_click(img_path='./img/gk_sd_r_qd_qd.png', name='返回港口商店')
+        for iter in range(5):
+            pag.moveTo(fpx, fpy)
+            pag.drag(DPM*2)
+
+        finished, _, _ = self.drag_find_and_click(fp=[fpx, fpy+2*DPM], dragto=[0, -2*DPM], offset=[6*DPM, 0.5*DPM], img_path='./img/gk_sd_o.png', name='橙色饰品碎片')
+        if finished:
+            self.find_and_click(img_path='./img/gk_sd_pt.png', name='+10')
+            self.find_and_click(img_path='./img/gk_sd_o_qd.png', name='确定购买橙色饰品碎片')
+            self.find_and_click(img_path='./img/gk_sd_o_qd_qd.png', name='返回港口商店')
+        for iter in range(5):
+            pag.moveTo(fpx, fpy)
+            pag.drag(DPM*2)
+
+        finished, _, _ = self.drag_find_and_click(fp=[fpx, fpy+2*DPM], dragto=[0, -2*DPM], offset=[6*DPM, 0.5*DPM], img_path='./img/gk_sd_kj.png', name='科技芯片')
+        if finished:
+            self.find_and_click(img_path='./img/gk_sd_pt.png', name='+10')
+            self.find_and_click(img_path='./img/gk_sd_kj_qd.png', name='确定购买科技芯片')
+            self.find_and_click(img_path='./img/gk_sd_kj_qd_qd.png', name='返回港口商店')
+
+        self.find_and_click(img_path='./img/gk_sd_fh.png', name='退出港口商店')
+        self.find_and_click(img_path='./img/gk_fh.png', name='退出港口')
+        if finished:
+            print('    港口商店完成')
+
 
 ar = AutoRun()
 ar.run()
+
